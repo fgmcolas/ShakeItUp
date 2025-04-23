b299a433471a734fdd1393671936bfcaad00ea9c
fgmcolas <fgmcolas@hotmail.com>
2025-04-23 21:48:58 +0200

    add route meta and guard logic for auth and guest

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, ".env") });
+
+import authRoutes from "./routes/auth.routes.js";
+import cocktailRoutes from "./routes/cocktail.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/cocktails", cocktailRoutes);
+app.use("/api/users", userRoutes);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Backend is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => console.error("MongoDB connection error:", err));
