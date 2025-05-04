e1659f14c064e23fb16a4cf7c6f7ff762dc29db0
fgmcolas <fgmcolas@hotmail.com>
2025-05-04 23:18:21 +0200

    Add conditional display of FavoriteButton for logged-in users

--- frontend/src/components/CocktailCard.vue
+++ frontend/src/components/CocktailCard.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from 'vue';
 import Badge from './Badge.vue';
+import FavoriteButton from './FavoriteButton.vue';
+import { useAuth } from '../composables/useAuth';
 
 const props = defineProps({
-    cocktail: Object
+    cocktail: Object,
 });
+
+const auth = useAuth();
 
 const fallbackImage = '/default-cocktail.jpg';
 const imageSrc = ref(props.cocktail.image);
@@ -15,10 +19,16 @@ const handleImageError = () => {
 </script>
 
 <template>
-    <div class="bg-[#1e1e1e] rounded-lg shadow-lg overflow-hidden hover:bg-[#2a2a2a] transition duration-200">
-        <!-- Image wrapper -->
+    <div class="relative bg-[#1e1e1e] rounded-lg shadow-lg overflow-hidden hover:bg-[#2a2a2a] transition duration-200">
+
+        <!-- Image -->
         <div class="w-full h-40 overflow-hidden bg-black">
             <img :src="imageSrc" :alt="cocktail.name" class="w-full h-full object-cover" @error="handleImageError" />
+        </div>
+
+        <!-- Favorite button only if user is logged in -->
+        <div v-if="auth.user.value" class="absolute top-2 right-2 z-10">
+            <FavoriteButton :cocktailId="cocktail._id" />
         </div>
 
         <div class="p-4 space-y-2">
