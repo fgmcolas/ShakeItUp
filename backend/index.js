faddc12424bb6ab6ea6991bada50e7c4951099fe
fgmcolas <fgmcolas@hotmail.com>
2025-05-04 22:00:15 +0200

    Connect backend to MongoDB using Mongoose

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
