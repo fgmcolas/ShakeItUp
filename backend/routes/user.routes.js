edce04aa0f56e1b6a4d5eb8219e877dadea6211a
fgmcolas <fgmcolas@hotmail.com>
2025-05-04 23:17:55 +0200

    Secure favorite and ingredient routes with token check

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { getUserById, updateFavorites, updateIngredients } from "../controllers/user.controller.js";
+import {
+    getUserById,
+    updateFavorites,
+    updateIngredients,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
