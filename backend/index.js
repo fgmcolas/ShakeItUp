d46fa0c490cf127539e5b4e6cc2321e4c07d5746
fgmcolas <fgmcolas@hotmail.com>
2025-05-06 01:47:33 +0200

    Wire updated rating routes and controllers into Express app

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -14,11 +14,12 @@ dotenv.config({ path: join(__dirname, ".env") });
 import authRoutes from "./routes/auth.routes.js";
 import cocktailRoutes from "./routes/cocktail.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import ratingRoutes from "./routes/rating.routes.js";
 
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json());
@@ -27,6 +28,7 @@ app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
 app.use("/api/cocktails", cocktailRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/ratings", ratingRoutes);
 
 mongoose
     .connect(process.env.MONGO_URI)
