a1e171b5457fbc7347d54f013892814950eaa21f
fgmcolas <fgmcolas@hotmail.com>
2025-05-08 19:26:38 +0200

    Add route to fetch cocktail details by ID

--- backend/routes/cocktail.routes.js
+++ backend/routes/cocktail.routes.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { getAllCocktails } from "../controllers/cocktail.controller.js";
+import multer from "multer";
+import { getAllCocktails, createCocktail } from "../controllers/cocktail.controller.js";
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + file.originalname.replace(/\s+/g, '_');
+        cb(null, uniqueSuffix);
+    },
+});
+const upload = multer({ storage });
+
 router.get("/", getAllCocktails);
+router.post("/", upload.single("image"), createCocktail);
 
 export default router;
