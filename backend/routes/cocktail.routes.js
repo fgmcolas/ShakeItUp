69b03d78f3999adacfb412ec6af47eff6b040f9c
fgmcolas <fgmcolas@hotmail.com>
2025-05-08 23:18:23 +0200

    Enable file upload in cocktail creation route

--- backend/routes/cocktail.routes.js
+++ backend/routes/cocktail.routes.js
@@ -1,9 +1,12 @@
-import express from "express";
-import { getAllCocktails, getCocktailById } from "../controllers/cocktail.controller.js";
+import express from 'express';
+import multer from 'multer';
+import { createCocktail, getAllCocktails, getCocktailById } from '../controllers/cocktail.controller.js';
 
 const router = express.Router();
+const upload = multer({ dest: 'uploads/' });
 
-router.get("/", getAllCocktails);
-router.get("/:id", getCocktailById);
+router.post('/', upload.single('image'), createCocktail);
+router.get('/', getAllCocktails);
+router.get('/:id', getCocktailById);
 
 export default router;
