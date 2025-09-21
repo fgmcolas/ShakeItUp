da73f45909b69068c1b448b1247615ce93e90b8a
fgmcolas <fgmcolas@hotmail.com>
2025-09-21 18:23:47 +0200

    Add comments and clarify sidebar state in sidebar.js

--- frontend/src/stores/sidebar.js
+++ frontend/src/stores/sidebar.js
@@ -1,8 +1,10 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
+// Simple UI store for the sidebar open/close state
 export const useSidebarStore = defineStore('sidebar', () => {
     const isOpen = ref(false);
+
     const toggle = () => (isOpen.value = !isOpen.value);
     const close = () => (isOpen.value = false);
     const open = () => (isOpen.value = true);
